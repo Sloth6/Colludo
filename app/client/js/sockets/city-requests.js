6deeba8ca5378ccc5f1683e578a8aa44abba3ccb
Daniel Belchamber <daniel.belchamber@gmail.com>
2013-08-24 17:45:58 -0400

    I Done Fucked up!

--- app/client/js/sockets/city-requests.js
+++
@@ -1,9 +0,0 @@
-// The client tells the server what it wants to buy.
-function serverBuy(tileType, tiles) {
-	socket.emit('orderCityTiles', {'tileType' : tileType, 'tiles' : tiles, 'cityId' : player.cities[0]});
-}
-
-
-function serverOrderTroops(cityId, troopType, n) {
-	socket.emit('orderTroops', {'cityId' : cityId, 'troopType' : troopType, 'n' : n});
-}
--- app/client/js/sockets/onLoad-requests.js
+++
@@ -1,3 +0,0 @@
-function onLoadRequests(){
-	socket.emit('requestInitData', {});
-}
