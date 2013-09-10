dfa334d83b8bec0fc956e5cf912b857064d1dadc
Daniel Belchamber <daniel.belchamber@gmail.com>
2013-09-10 15:34:20 -0400

    Obiwan has arrived!

--- app/client/js/utilities.js
+++ app/client/js/utilities.js
@@ -1,7 +1,7 @@
 function assert(condition, message) {
-    if (!condition) {
-        throw message || "Assertion failed";
-    }
+	if (!condition) {
+		throw message || "Assertion failed";
+	}
 }
 // name that panel!
 function getCurrentPanel() {
@@ -13,30 +13,30 @@ function getCurrentPanel() {
 function loadImages(tilePath, imageNames, imageArr, callback) {
 	var loadedImagesCount = 0;
 	for (var i = 0; i < imageNames.length; i++) {
-	    var image = new Image();
-	    image.src = tilePath+imageNames[i];
-	    image.onload = function(){
-	        loadedImagesCount++;
-	        if (loadedImagesCount >= imageNames.length) {	            
+		var image = new Image();
+		image.src = tilePath+imageNames[i];
+		image.onload = function(){
+			loadedImagesCount++;
+			if (loadedImagesCount >= imageNames.length) {	            
 				//Get update the player model with server info.
 				callback();
-	        }
-	    }
-	    imageArr.push(image);
+			}
+		}
+		imageArr.push(image);
 	}
 }
 function loadImages2(tilePath, imageNames, imageArr, callback) {
 	var loadedImagesCount = 0;
 	for (var i = 0; i < imageNames.length; i++) {
-    var image = new Image();
-    image.src = tilePath+imageNames[i]+'.png';
-    image.onload = function(){
-    	loadedImagesCount++;
-    	if (loadedImagesCount >= imageNames.length) {	            
+		var image = new Image();
+		image.src = tilePath+imageNames[i]+'.png';
+		image.onload = function() {
+			loadedImagesCount++;
+			if (loadedImagesCount >= imageNames.length) {	            
 				//Get update the player model with server info.
 				callback();
-       }
-    }
-    imageArr[imageNames[i]] = image;
+			}
+		}
+		imageArr[imageNames[i]] = image;
 	}
 }
