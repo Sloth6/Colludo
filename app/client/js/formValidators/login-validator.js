e0219c402396613df66c5c974bccb5510e02cafc
Daniel Belchamber <daniel.belchamber@gmail.com>
2013-09-12 14:24:19 -0400

    Alert messages for typing in the wrong email/password.

--- app/client/js/formValidators/login-validator.js
+++ app/client/js/formValidators/login-validator.js
@@ -13,6 +13,20 @@ function loginUser(form) {
 		success:  function(responseText, status) {
 			console.log('manual-login success!');
 			window.location = SERVER+'/game';
+		},
+		error:    function(response) {
+			console.log('received error:', response.responseText);
+			if (response.responseText == 'user-not-found') {
+				alert('There is no user attached to that email.');
+				form.email.value = '';
+				form.password.value = '';
+				form.email.focus();
+			}
+			else if (response.responseText == 'invalid-password') {
+				alert('The password you gave was incorrect.');
+				form.password.value = '';
+				form.password.focus();
+			}
 		}
 	});
 }
